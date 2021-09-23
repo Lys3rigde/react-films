3f49c89a8185407b529bf94c158626383b2eb8a9
Egor Matveev <egormsm@gmail.com>
2021-09-23 16:11:51 +0300

    added link to Repository in header & footer

--- src/layout/Footer.jsx
+++ src/layout/Footer.jsx
@@ -1,14 +1,14 @@
 function Footer() {
-    return (
-        <footer className="page-footer deep-purple lighten-1">
-          <div className="footer-copyright deep-purple lighten-1">
-            <div className="container">
-            © {new Date().getFullYear()} Copyright Text
-            <a className="grey-text text-lighten-4 right" href="!#">Repo</a>
-            </div>
-          </div>
-        </footer>
-    )
+  return (
+    <footer className="page-footer deep-purple lighten-1">
+      <div className="footer-copyright deep-purple lighten-1">
+        <div className="container">
+          © {new Date().getFullYear()} Copyright Text
+          <a className="grey-text text-lighten-4 right" href="https://github.com/Lys3rigde/react-films">Repo</a>
+        </div>
+      </div>
+    </footer>
+  )
 }
 
-export {Footer}
+export { Footer }
