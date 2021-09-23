a8c2c04fd598ad92e7a2d856eed9618579270364
Egor Matveev <egormsm@gmail.com>
2021-09-23 16:16:42 +0300

    fixed repo link in header

--- src/layout/Header.jsx
+++ src/layout/Header.jsx
@@ -2,9 +2,9 @@ function Header() {
   return (
     <nav className="deep-purple lighten-1">
       <div className="nav-wrapper">
-        <a href="https://github.com/Lys3rigde/react-films" className="brand-logo">React Movies</a>
+        <a href="!#" className="brand-logo">React Movies</a>
         <ul id="nav-mobile" className="right hide-on-med-and-down">
-          <li><a href="!#">Repo</a></li>
+          <li><a href="https://github.com/Lys3rigde/react-films">Repo</a></li>
         </ul>
       </div>
     </nav>
